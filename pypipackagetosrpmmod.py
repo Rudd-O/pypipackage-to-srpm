f7d57db0456a6596c167e5f1fc2dc65e96faa7c8
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2018-10-17 04:57:30 +0000

    Proper mangling of versions.

---
+++ pypipackagetosrpmmod.py
@@ -0,0 +1,52 @@
+import sys
+
+
+def mangle_name(name):
+  # special case dnspython.
+  if name == "dnspython":
+      return "python%s-dns" % (sys.version_info.major, )
+  if name == "BeautifulSoup4":
+      name = "beautifulsoup4"
+  if name.startswith("python"):
+    return name
+  # Special case pytz.
+  if name.startswith("pytz"):
+    return "python%s-%s" % (sys.version_info.major, name)
+  if name.startswith("PySocks") or name.startswith("pyaes"):
+    return "python%s-%s" % (sys.version_info.major, name.lower())
+  if name.startswith("py"):
+    return "python%s-%s" % (sys.version_info.major, name[2:])
+  return "python%s-%s" % (sys.version_info.major, name)
+
+
+def gen_requires(in_reqs):
+  requires = []
+  for l in in_reqs:
+    l = l.strip()
+    l = l.replace(" ", "")
+    if not l:
+      continue
+    if l.startswith("["):
+        break
+    toks = l.split(",")
+    pkg = None
+    for tok in toks:
+        seps = [">=", "<=", ">", "<", "==", None]
+        ver = None
+        for sep in seps:
+            if sep is None:
+                pass
+            elif sep in tok:
+                tok, ver = tok.split(sep)
+                tok, ver = tok.strip(), ver.strip()
+                break
+        assert not (ver is None and sep is not None)
+        assert not (sep is None and ver is not None)
+        if tok.strip():
+            pkg = tok.strip()
+        if ver is None:
+            dep = mangle_name(pkg)
+        else:
+            dep = mangle_name(pkg) + " " + sep + " " + ver
+        requires.append(dep)
+  return requires
---
+++ test_pypipackagetosrpmmod.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import unittest
+
+
+sys.path.append(os.path.dirname(__file__))
+
+
+import pypipackagetosrpmmod as mod
+
+
+class TestRequires(unittest.TestCase):
+
+    def test_weird_requires(self):
+        in_ = """httplib2<1dev,>=0.9.2
+google-auth>=1.4.1
+google-auth-httplib2>=0.0.3
+six<2dev,>=1.6.1
+uritemplate<4dev,>=3.0.0""".splitlines()
+        exp_ = [
+            "python2-httplib2 < 1dev",
+            "python2-httplib2 >= 0.9.2",
+            "python2-google-auth >= 1.4.1",
+            "python2-google-auth-httplib2 >= 0.0.3",
+            "python2-six < 2dev",
+            "python2-six >= 1.6.1",
+            "python2-uritemplate < 4dev",
+            "python2-uritemplate >= 3.0.0",
+        ]
+        out_ = mod.gen_requires(in_)
+        self.assertEqual(exp_, out_)
