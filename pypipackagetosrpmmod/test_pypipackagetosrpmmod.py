fdc5e8d385c82da30bb873c569f1c992c69eac82
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2019-06-12 00:06:40 +0000

    Tag 0.0.21.

--- pypipackagetosrpmmod/test_pypipackagetosrpmmod.py
+++ pypipackagetosrpmmod/test_pypipackagetosrpmmod.py
@@ -4,7 +4,7 @@ import unittest
 
 
 sys.path.append(os.path.dirname(__file__))
-
+pv = sys.version_info.major
 
 import pypipackagetosrpmmod as mod
 
@@ -19,18 +19,18 @@ someshit~=0.0.4
 six<2dev,>=1.6.1
 uritemplate<4dev,>=3.0.0""".splitlines()
         exp_ = [
-            "python2-httplib2 < 1dev",
-            "python2-httplib2 >= 0.9.2",
-            "python2-google-auth >= 1.4.1",
-            "python2-google-auth-httplib2 >= 0.0.3",
-            "python2-someshit >= 0.0.4",
-            "python2-six < 2dev",
-            "python2-six >= 1.6.1",
-            "python2-uritemplate < 4dev",
-            "python2-uritemplate >= 3.0.0",
+            "python%s-httplib2 < 1dev" % pv,
+            "python%s-httplib2 >= 0.9.2" % pv,
+            "python%s-google-auth >= 1.4.1" % pv,
+            "python%s-google-auth-httplib2 >= 0.0.3" % pv,
+            "python%s-someshit >= 0.0.4" % pv,
+            "python%s-six < 2dev" % pv,
+            "python%s-six >= 1.6.1" % pv,
+            "python%s-uritemplate < 4dev" % pv,
+            "python%s-uritemplate >= 3.0.0" % pv,
         ]
         out_ = mod.gen_requires(in_)
         self.assertEqual(exp_, out_)
 
     def test_mangle_name_enter(self):
-        assert mod.mangle_name("Babel\n") == "python%s-babel" % sys.version_info.major
+        assert mod.mangle_name("Babel\n") == "python%s-babel" % pv
